c1d73c237525e6685486be89392a51aee6991b4b
apurba4u <apurba185loser@gmail.com>
2026-01-03 21:23:01 +0600

    Error in code

--- project1/src/components/ResultGrid.jsx
+++ project1/src/components/ResultGrid.jsx
@@ -1,35 +1,67 @@
-import axios from "axios"
-import { useSelector } from "react-redux"
-import { fetchGif, fetchPhotos, fetchVideos } from "../api/mediaApi"
-import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGif, fetchPhotos, fetchVideos } from "../api/mediaApi";
+import { useEffect } from "react";
+import { setError, setLoading, setResults } from "../redux/features/searchSlice";
+
 
 const ResultGrid = () => {
-  const {query, activeTab, results, loading, error} = useSelector((store) => store.search)
+  const { query, activeTab, results, loading, error } = useSelector(
+    (store) => store.search
+  );
+  const dispatch = useDispatch();
 
-  useEffect(function () {
-    async function getData() {
-      let data
-      if (activeTab == 'photos') {
-        let response = await fetchPhotos(query)
-        data = response.results
+  useEffect(
+    function () {
+      if(!query) return
+      async function getData() {
+        try {
+          dispatch(setLoading())
+          let data = [];
+          if (activeTab == "photos") {
+            let response = await fetchPhotos(query);
+            data = response.results.map((elem) => ({
+              id: elem.id,
+              type: "photo",
+              title: elem.alt_description,
+              thumbnail: elem.urls.small,
+              src: elem.urls.full,
+            }));
+          }
+          if (activeTab == "videos") {
+            let response = await fetchVideos(query);
+            data = response.videos.map((elem) => ({
+              id: elem.id,
+              type: "video",
+              title: elem.user.name || "video",
+              thumbnail: elem.image,
+              src: elem.video_files[0].link,
+            }));
+          }
+          if (activeTab == "gif") {
+            let response = await fetchGif(query);
+            data = response.data.results.map((elem) => ({
+              id: elem.id,
+              type: "GIF",
+              title: elem.title || "GIF",
+              src: elem.media_formats.gif.url,
+              thumbnail: elem.media_formats.tinygif.url,
+            }));
+          }
+          dispatch(setResults(data));
+        } catch (err) {
+          dispatch(setError(err.message))
+        }
       }
-      if (activeTab == 'videos') {
-        let response = await fetchVideos(query)
-        data = response.videos
-      }
-      if (activeTab == 'gif') {
-        let response = await fetchGif(query)
-        data = response.data.results
-      }
-      console.log(data);
-    }
-    getData()
-  }, [query, activeTab])
-  return (
-    <div>
-      {/* <button onClick={getData}>Click</button> */}
-    </div>
-  )
-}
+      getData();
+    },
+    [query, activeTab])
+  if (error) return <h1>Error</h1>
+  if (loading) return <h1>Loading</h1>
+  return <div>
+    {results.map((elem, idx) => {
+      return <div key={idx}>{elem.title}</div>
+    })}
+  </div>;
+};
 
-export default ResultGrid
+export default ResultGrid;
