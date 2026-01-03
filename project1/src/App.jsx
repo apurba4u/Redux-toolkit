70c6248d6944669f3aed8415fbe037fb2fd2c806
apurba4u <apurba185loser@gmail.com>
2026-01-03 17:12:49 +0600

    Fetch Photos

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,8 +1,15 @@
+import { fetchPhotos } from "./api/mediaApi"
 
 const App = () => {
+  // function getPhotos() {
+  //   fetchPhotos()
+  // }
   return (
     <div className="h-screen w-full bg-gray-950 text-white">
-      <h1>App</h1>
+      <button onClick={async () => {
+        const data = await fetchPhotos('dog')
+        console.log(data);
+      }}>Click</button>
     </div>
   )
 }
