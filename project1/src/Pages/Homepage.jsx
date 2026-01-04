7c5214c78e60e2f4d52ca883292d52c188ba7c6f
apurba4u <apurba185loser@gmail.com>
2026-01-04 13:28:19 +0600

    query nah takle value search value show korbe nah

--- project1/src/Pages/Homepage.jsx
+++ project1/src/Pages/Homepage.jsx
@@ -1,15 +1,20 @@
-
-import ResultGrid from "../components/ResultGrid"
-import { SearchBar } from "../components/SearchBar"
-import { Tabs } from "../components/Tabs"
+import { useSelector } from "react-redux";
+import ResultGrid from "../components/ResultGrid";
+import { SearchBar } from "../components/SearchBar";
+import { Tabs } from "../components/Tabs";
 
 export const Homepage = () => {
+  const { query } = useSelector((store) => store.search);
   return (
     <div>
-      <div className="text-center p-5 bg-blue-900 text-2xl font-semibold">Media Search</div>
+      <div className="text-center p-5 bg-blue-900 text-2xl font-semibold">
+        Media Search
+      </div>
       <SearchBar />
-      <Tabs />
-      <ResultGrid />
+      {query != '' ? <div>
+        <Tabs />
+        <ResultGrid />
+      </div> : ''}
     </div>
-  )
-}
+  );
+};
